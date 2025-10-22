f3b27822e609a8d7c51e2a0801c540d561e74b76
Nikita Vakula <programmistov.programmist@gmail.com>
2025-10-22 12:20:52 +0200

    Fix start script for arm64
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- example/start-vm.sh
+++ example/start-vm.sh
@@ -7,6 +7,7 @@ QEMU_LOGS=/tmp/qemu.logs
 cleanup() {
     trap - SIGINT SIGTERM ERR EXIT
     # script cleanup here
+    cat ${QEMU_LOGS}
     rm -fr ${QEMU_LOGS}
 }
 
@@ -21,8 +22,17 @@ if [ "$(uname)" == "Darwin" ]; then
     ACCEL="hvf"
 fi
 
-qemu-system-x86_64 \
-    -machine q35 -accel ${ACCEL} -m 2048 -nographic \
+ARCH=$(uname -m)
+if [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
+    QEMU_BIN="qemu-system-aarch64"
+    MACHINE_OPTS="-machine virt -cpu cortex-a57"
+else
+    QEMU_BIN="qemu-system-x86_64"
+    MACHINE_OPTS="-machine q35"
+fi
+
+${QEMU_BIN} \
+    ${MACHINE_OPTS} -accel ${ACCEL} -m 2048 -nographic \
     -hda ./${IMAGE} \
     -nographic \
     -qmp "unix:/tmp/example.qmp,server,wait=off" > ${QEMU_LOGS} 2>&1
